fa0fa71708cc8a28124827596f9958c064f004b7
Alexander Baryshnikov <dev@baryshnikov.net>
2020-05-15 14:53:24 +0800

    ignore args in sudo

--- runner/sudo_linux.go
+++ runner/sudo_linux.go
@@ -11,14 +11,19 @@ var queries = []string{
 
 func withSudo(args []string) []string {
 	var sudo = "sudo"
+	var dash bool
 	for _, name := range queries {
 		if _, err := exec.LookPath(name); err == nil {
 			sudo = name
+			dash = true
 			break
 		}
 	}
 	var ans []string
 	ans = append(ans, sudo)
+	if dash {
+		ans = append(ans, "--")
+	}
 	ans = append(ans, args...)
 	return ans
 }
