5a7bfe124f97f4c5b6beb1558dec31c670364dd3
Alexander Baryshnikov <dev@baryshnikov.net>
2020-05-15 13:20:00 +0800

    add list networks in dir

--- network/helpers.go
+++ network/helpers.go
@@ -1,6 +1,9 @@
 package network
 
-import "io/ioutil"
+import (
+	"io/ioutil"
+	"path/filepath"
+)
 
 func ConfigFromFile(name string) (*Config, error) {
 	var cfg Config
@@ -9,4 +12,23 @@ func ConfigFromFile(name string) (*Config, error) {
 		return nil, err
 	}
 	return &cfg, cfg.Parse(data)
+}
+
+// List networks in directory
+func List(directory string) ([]Network, error) {
+	abs, err := filepath.Abs(directory)
+	if err != nil {
+		return nil, err
+	}
+	list, err := ioutil.ReadDir(directory)
+	if err != nil {
+		return nil, err
+	}
+	var ans []Network
+	for _, item := range list {
+		if item.IsDir() && IsValidName(item.Name()) {
+			ans = append(ans, Network{Root: filepath.Join(abs, item.Name())})
+		}
+	}
+	return ans, nil
 }
